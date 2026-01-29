5398ff1f6e091c18fd43871455b86171329368f2
arivermaNokia <113878198+arivermaNokia@users.noreply.github.com>
2026-01-29 20:36:38 +0530

    Add files via upload

---
+++ Assigment1_Task2.py
@@ -0,0 +1,10 @@
+from queue import PriorityQueue
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+
+str11='!'
+str2= last_name + str11
+
+print("Hello,",first_name,str2,"Welcome to the Python program.")
